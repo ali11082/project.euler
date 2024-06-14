e54ef3697cc002e575133f31cee3790832ab2502
ali1100 <aliyazdani0110@gmail.com>
2024-06-14 11:09:44 -0700

    edit code

--- p02.01.01.py
+++ p02.01.01.py
@@ -1,17 +1,9 @@
-
-
-
-
-
-a=0
+#بین 0 تا 100 عددی حدس زده میشود و از کاربر درستی عدد حدس زده شده سوال پرسیده می شود
 import random
 h1=0
 h2=99
-#h=random.randint(h1,h2)
 r="felan hichi"
-#print("man hadsam =",h, " bood")
-#r=input("age adadam bozorgtare benevis b va age hadsam koochiktar boodesh benvis k va age dorost goftam benevis ok:   ")
-while (r!="ok"):
+while r!="ok":
     h=random.randint(h1,h2)
     print("man hadsam =",h," chetore?")
     r=input("age adadam bozorgtare benevis b va age hadsam koochiktar boodesh benvis k va age dorost goftam benevis ok:   ")
@@ -23,5 +15,3 @@ while (r!="ok"):
         h2=h-1
     else:
         print("vorodit motabar nabood pas adad jadid hads mizanam")
-#if r=="ok":
- #   print("tamam tamam")
